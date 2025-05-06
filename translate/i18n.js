a15f7ca39426d44812e7451100f4353879514abf
Lucas Alves Marques <160551088+Lucas-Alves-Marques@users.noreply.github.com>
2025-05-05 21:50:56 -0300

    Adição do Projeto ao Portifolio

---
+++ translate/i18n.js
@@ -0,0 +1,22 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import ptBR from './br.json';
+import { compatibilityFlags } from 'react-native-screens';
+
+i18n.use(initReactI18next).init({
+
+    compatibilityJSON: 'v3',
+    lng:'pt-BR',
+    resources: {
+        'pt-BR' : ptBR,
+    },
+    react: {
+        useSuspense: false,
+    },
+    interpolation:{
+        escapeValue: false,
+    }
+})
+
+export default i18n;
